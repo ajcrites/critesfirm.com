50bea076ae2653921614f7c3986c661f24d9e3aa
Andrew Crites <acrites@mobiquityinc.com>
2022-09-29 19:43:38 -0400

    add analytics and set up firebase for hosting/deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAX29XwaFXPxuw2HJX07L_X3Qb4_i0GKbg",
+  projectId: "critesfirmcom",
+  appId: "1:741886314396:web:e805a3a401dd613ae92a59",
+};
+
+const app = initializeApp(firebaseConfig);
+if (process.env.NEXT_PUBLIC_HAS_ANALYTICS) {
+  isSupported().then(() => getAnalytics(app));
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
